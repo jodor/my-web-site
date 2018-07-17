e099afb7cad5468db1f5ba31d900ebc20eb16ddc
DORSO Josselin <josselin.dorso@reseau.eseo.fr>
2018-07-17 16:07:53 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { CvComponent } from './cv/cv.component';
+import { CareerComponent } from './career/career.component';
+import { ContactComponent } from './contact/contact.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+
+const appRoutes: Routes = [
+    { path: 'cv', component: CvComponent },
+    { path: 'career', component: CareerComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '', component: CvComponent },
+    { path: 'not-found', component: FourOhFourComponent },
+    { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CvComponent,
+        CareerComponent,
+        ContactComponent,
+        FourOhFourComponent
+    ],
+    imports: [
+        BrowserModule,
+        UiModule,
+        NgbCollapseModule,
+        FormsModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+
+    onSubmit(form: NgForm) {
+        console.log(form.value);
+    }
+
+}
